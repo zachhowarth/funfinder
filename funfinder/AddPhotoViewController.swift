046f4fd8fdb101125d5dbdf3ea458f678a276e99
Zach Howarth <zachhowarth@gmail.com>
2019-05-30 14:40:27 -0500

    Iteration 2 Complete

--- funfinder/AddPhotoViewController.swift
+++ funfinder/AddPhotoViewController.swift
@@ -12,7 +12,8 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
     
     var imagePicker = UIImagePickerController()
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var photoToSave: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,11 +44,30 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
         present(imagePicker, animated: true, completion: nil)
     }
     
+    @IBOutlet weak var captionText: UITextField!
+    
+    @IBAction func savePhotoTapped(_ sender: UIButton) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            let newCDPhoto = Photos(entity: Photos.entity(), insertInto: context)
+            newCDPhoto.caption = captionText.text
+            
+            if let userImage = photoToSave.image {
+                if let userImageData = userImage.pngData() {
+                    newCDPhoto.addPhoto = userImageData
+                }
+            }
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+            navigationController?.popViewController(animated: true)
+        }
+        
+    }
+    
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         //update photo
         
         if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            imageView.image = selectedImage
+            photoToSave.image = selectedImage
             
         }
         imagePicker.dismiss(animated: true, completion: nil )
@@ -59,3 +79,4 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
 
 
 }
+
---
+++ funfinder/PhotoTableViewController.swift
@@ -0,0 +1,101 @@
+//
+//  PhotoTableViewController.swift
+//  funfinder
+//
+//  Created by Zachary Howarth on 5/19/19.
+//  Copyright © 2019 Zachary Howarth. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewController: UITableViewController {
+    //this property starts as an empty array, but will eventually have Photos from CD.
+    var photos : [Photos] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // this function will be called right before the view appears.
+    override func viewWillAppear(_ animated: Bool) {
+        // so, inside it we call the getphotos function to get our most up-to-date CD photos!
+        getPhotos()
+    }
+    
+    // this function will retrieve all our photos from CD
+    func getPhotos() {
+    
+    if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+    
+    if let coreDataPhotos = try? context.fetch(Photos.fetchRequest()) as? [Photos] {
+    photos = coreDataPhotos
+    tableView.reloadData()
+    }
+    }
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let cellPhoto = photos[indexPath.row]
+        
+        cell.textLabel?.text = cellPhoto.caption
+        
+        if let cellPhotoImageData = cellPhoto.addPhoto {
+            if let cellPhotoImage = UIImage(data: cellPhotoImageData) {
+                cell.imageView?.image = cellPhotoImage
+            }
+        }
+        
+        return cell
+    }
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
